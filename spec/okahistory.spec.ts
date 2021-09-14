24e424cc064774c8afe2f7c734519f9037d79ebc
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-09-14 23:17:34 +0900

    feat: create history module

--- spec/okahistory.spec.ts
+++ spec/okahistory.spec.ts
@@ -1,9 +1,115 @@
 import { useHistory } from '../src/okahistory'
 
-describe('okahistory', () => {
-  describe('useHistory', () => {
-    it('TODO', () => {
-      expect(useHistory()).toEqual({})
+describe('useHistory', () => {
+  function setup() {
+    const target = useHistory()
+    const state = { value: 0 }
+
+    target.registReducer('ope_a', {
+      undo(before: number) {
+        state.value = before
+      },
+      redo(after: number) {
+        const before = state.value
+        state.value = after
+        return before
+      },
+    })
+
+    return {
+      target,
+      state,
+    }
+  }
+
+  it('should throw if unknown action is called', () => {
+    const { target } = setup()
+    expect(() =>
+      target.execAction({
+        name: 'unknown',
+        args: 10,
+      })
+    ).toThrow('not found the action: unknown')
+  })
+
+  it('should save actions', () => {
+    const { target, state } = setup()
+
+    target.execAction({
+      name: 'ope_a',
+      args: 10,
+    })
+    expect(state.value).toBe(10)
+
+    target.execAction({
+      name: 'ope_a',
+      args: 20,
+    })
+    expect(state.value).toBe(20)
+  })
+
+  describe('undo & redo', () => {
+    it('should undo & redo', () => {
+      const { target, state } = setup()
+
+      target.execAction({
+        name: 'ope_a',
+        args: 10,
+      })
+      target.execAction({
+        name: 'ope_a',
+        args: 20,
+      })
+
+      expect(state.value).toBe(20)
+      expect(target.getCurrentIndex()).toBe(1)
+      target.undo()
+      expect(state.value).toBe(10)
+      expect(target.getCurrentIndex()).toBe(0)
+      target.undo()
+      expect(state.value).toBe(0)
+      expect(target.getCurrentIndex()).toBe(-1)
+      target.undo()
+      expect(state.value).toBe(0)
+      expect(target.getCurrentIndex()).toBe(-1)
+      target.redo()
+      expect(state.value).toBe(10)
+      expect(target.getCurrentIndex()).toBe(0)
+      target.redo()
+      expect(state.value).toBe(20)
+      expect(target.getCurrentIndex()).toBe(1)
+      target.redo()
+      expect(state.value).toBe(20)
+      expect(target.getCurrentIndex()).toBe(1)
+    })
+
+    it('should dispose redo branch if new action is pushed', () => {
+      const { target, state } = setup()
+
+      target.execAction({
+        name: 'ope_a',
+        args: 10,
+      })
+      target.execAction({
+        name: 'ope_a',
+        args: 20,
+      })
+      expect(state.value).toBe(20)
+      target.undo()
+      expect(state.value).toBe(10)
+      target.execAction({
+        name: 'ope_a',
+        args: 30,
+      })
+      expect(state.value).toBe(30)
+      target.redo()
+      expect(state.value).toBe(30)
+      target.undo()
+      expect(state.value).toBe(10)
+      target.undo()
+      expect(state.value).toBe(0)
+      target.undo()
+      expect(state.value).toBe(0)
     })
   })
 })
--- src/okahistory.ts
+++ src/okahistory.ts
@@ -1,3 +1,89 @@
-export function useHistory() {
-  return {}
+export type ActionName = string
+
+export interface Action<RedoArgs> {
+  name: ActionName
+  args: RedoArgs
+}
+
+export interface Reducer<RedoArgs, UndoArgs> {
+  redo(redoState: RedoArgs): UndoArgs
+  undo(undoState: UndoArgs): void
+}
+
+export interface SavedAction<RedoArgs, UndoArgs> {
+  name: ActionName
+  redoArgs: RedoArgs
+  undoArgs: UndoArgs
+}
+
+interface HistoryModule {
+  registReducer<UndoArgs, RedoArgs>(
+    name: ActionName,
+    reducer: Reducer<RedoArgs, UndoArgs>
+  ): void
+  execAction<RedoArgs>(action: Action<RedoArgs>): void
+  redo(): void
+  undo(): void
+  getCurrentIndex(): number
+}
+
+export function useHistory(): HistoryModule {
+  const reducerMap: { [name: ActionName]: Reducer<unknown, unknown> } = {}
+  const historyStack: SavedAction<unknown, unknown>[] = []
+  let currentStackIndex = -1
+
+  function registReducer<UndoArgs, RedoArgs>(
+    name: ActionName,
+    reducer: Reducer<RedoArgs, UndoArgs>
+  ) {
+    reducerMap[name] = reducer
+  }
+
+  function execAction<RedoArgs>(action: Action<RedoArgs>): void {
+    const reducer = reducerMap[action.name]
+    if (!reducer) throw new Error(`not found the action: ${action.name}`)
+
+    const undoArgs = reducer.redo(action.args)
+
+    pushHistory({
+      name: action.name,
+      redoArgs: action.args,
+      undoArgs,
+    })
+  }
+
+  function pushHistory(savedAction: SavedAction<unknown, unknown>): void {
+    if (currentStackIndex < historyStack.length - 1) {
+      historyStack.splice(
+        currentStackIndex + 1,
+        historyStack.length - currentStackIndex
+      )
+    }
+    historyStack.push(savedAction)
+    currentStackIndex = historyStack.length - 1
+  }
+
+  function redo() {
+    if (currentStackIndex < historyStack.length - 1) {
+      const current = historyStack[currentStackIndex + 1]
+      reducerMap[current.name].redo(current.redoArgs)
+      currentStackIndex = currentStackIndex + 1
+    }
+  }
+
+  function undo() {
+    if (-1 < currentStackIndex) {
+      const current = historyStack[currentStackIndex]
+      reducerMap[current.name].undo(current.undoArgs)
+      currentStackIndex = currentStackIndex - 1
+    }
+  }
+
+  return {
+    registReducer,
+    execAction,
+    redo,
+    undo,
+    getCurrentIndex: () => currentStackIndex,
+  }
 }
